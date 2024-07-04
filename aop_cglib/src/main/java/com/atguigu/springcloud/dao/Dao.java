4a55fa53cf38790d638ef75205b545e63b05824f
dingwenlong <gjsk-dingwenlong@sinomach.com.cn>
2024-07-04 10:28:59 +0800

    注释

--- aop_cglib/src/main/java/com/atguigu/springcloud/dao/Dao.java
+++ aop_cglib/src/main/java/com/atguigu/springcloud/dao/Dao.java
@@ -9,3 +9,9 @@ public class Dao {
         System.out.println("PeopleDao.select()");
     }
 }
+/**
+ * 1、有一个类有若干方法
+ * 2、代理类实现MethodInterceptor，重写intercept方法
+ * 3、enhancer关联父类和代理类
+ * 4、enhancer创建父类的子类，执行方法
+ */
